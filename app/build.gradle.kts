8527aab5065e062577215cdbca1f50bf3f9a1004
Y. Z. Chen <754097987@qq.com>
2026-09-01 21:47:32 +0800

    (Replace) markdown library with lightweight custom renderer.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -93,6 +93,4 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
 
     implementation("androidx.datastore:datastore-preferences:1.1.1")
-
-    implementation("com.mikepenz:multiplatform-markdown-renderer-m3-android:0.24.0")
 }
