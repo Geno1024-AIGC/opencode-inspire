f54a3e9a31166645dfbf9001f5cc2c9198124bdb
Y. Z. Chen <754097987@qq.com>
2026-09-04 07:18:29 +0800

    [Chore] Remove unused imports in stats screen

--- app/src/main/java/com/example/opencodeclient/ui/TokenCalendarScreen.kt
+++ app/src/main/java/com/example/opencodeclient/ui/TokenCalendarScreen.kt
@@ -4,7 +4,6 @@ import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -15,7 +14,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
